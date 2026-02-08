9fdc924208bc94405306aee0efad18cbd4430a96
vozikybrno <refo@email.cz>
2026-02-08 18:12:29 +0100

    Add files via upload
    Doladění SEO, úprava horního zeleného řádku, aby se text nelámal kde nemá, tel.č. jako link pro rychlé vytočení na mobilu.

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ function showSection(sectionId) {
   for (var i = 0; i < sections.length; i++) {
     sections[i].style.display = 'none';
   }
-  document.getElementById(sectionId).style.display = 'block';
+
+  var el = document.getElementById(sectionId);
+  if (el) {
+    el.style.display = 'block';
+  }
 }
